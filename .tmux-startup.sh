62abf6e89b5a9335125c839634259490fadf43ba
Yannik Peschke <yannik.peschke@gmail.com>
2026-01-10 18:29:15 +0100

    -

---
+++ .tmux-startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+tmux start-server
+
+SESSION_NAME="Work"
+
+tmux has-session -t "$SESSION_NAME" 2>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Session '$SESSION_NAME' already exists. Attaching..."
+  tmux attach -t "$SESSION_NAME"
+  exit 0
+fi
+
+tmux new-session -d -s "$SESSION_NAME" -n "dev" -c "$HOME/Development"
+
+tmux new-window -t "$SESSION_NAME":2 -n "docs" -c "$HOME/Docs"
+
+tmux attach -t "$SESSION_NAME"
