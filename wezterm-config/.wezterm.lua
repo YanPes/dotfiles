70f77049612f2b9d4393d047ba2f3e641ac858be
Yannik Peschke <yannik.peschke@gmail.com>
2026-01-10 18:29:30 +0100

    --

--- wezterm-config/.wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
-
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.window_background_opacity = 0.85
-
-return config
