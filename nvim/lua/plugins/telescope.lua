6187a3f5953a28631e50a0b01b26e835e501b636
Yannik Peschke <yannik.peschke@gmail.com>
2026-03-19 14:29:25 +0100

    fix telescope behavior for hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,65 @@
+local telescope_builtin = require("telescope.builtin")
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = function()
-      return {
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      hidden = true,
+      mappings = {
+        i = {
+          ["<C-j>"] = require("telescope.actions").move_selection_next,
+          ["<C-k>"] = require("telescope.actions").move_selection_previous,
         },
-      }
-    end,
+        n = {
+          ["<C-j>"] = require("telescope.actions").move_selection_next,
+          ["<C-k>"] = require("telescope.actions").move_selection_previous,
+        },
+      },
+    },
+  },
+
+  keys = {
+    {
+      "<leader><leader>",
+      function()
+        telescope_builtin.find_files({ hidden = true })
+      end,
+      desc = "Find files (root dir)",
+    },
+    {
+      "<leader>ff",
+      function()
+        telescope_builtin.find_files({ hidden = true })
+      end,
+      desc = "Find files (root dir)",
+    },
+    {
+      "<leader>fF",
+      function()
+        telescope_builtin.find_files({ cwd = false, hidden = true })
+      end,
+      desc = "Find files (cwd)",
+    },
+    {
+      "<leader>/",
+      function()
+        telescope_builtin.live_grep({
+          cwd = false,
+          hidden = true,
+          additional_args = {
+            "--hidden",
+            "--no-ignore",
+            "--glob",
+            "!.git/",
+            "--glob",
+            "!**/node_modules/**",
+          },
+        })
+      end,
+      desc = "Grep in cwd",
+    },
   },
 }
